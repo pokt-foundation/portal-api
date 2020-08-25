83db57f892e645208cf37255c6d6863e9c00f265
Alex Firmani <jafirmani@gmail.com>
2020-08-25 13:14:21 -0700

    changing timeout to 60s

--- src/application.ts
+++ src/application.ts
@@ -84,7 +84,7 @@ export class PocketGatewayApplication extends BootMixin(
       5, 
       100000, 
       5, 
-      20000, 
+      60000, 
       false,
       pocketSessionBlockFrequency,
       pocketBlockTime,
