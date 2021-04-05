6b29e183b0cdabe02fc3b1be87c47fba6670a748
Alex Firmani <jafirmani@gmail.com>
2021-04-05 08:20:34 -0700

    moving pocket js to each thread; singapore only

--- dist/sequence.d.ts
+++ dist/sequence.d.ts
@@ -1,10 +1,15 @@
 import { FindRoute, InvokeMethod, ParseParams, Reject, RequestContext, Send, SequenceHandler } from '@loopback/rest';
 export declare class GatewaySequence implements SequenceHandler {
+    private dispatchURL;
+    private pocketSessionBlockFrequency;
+    private pocketBlockTime;
+    private clientPrivateKey;
+    private clientPassphrase;
     protected findRoute: FindRoute;
     protected parseParams: ParseParams;
     protected invoke: InvokeMethod;
     send: Send;
     reject: Reject;
-    constructor(findRoute: FindRoute, parseParams: ParseParams, invoke: InvokeMethod, send: Send, reject: Reject);
+    constructor(dispatchURL: string, pocketSessionBlockFrequency: number, pocketBlockTime: number, clientPrivateKey: string, clientPassphrase: string, findRoute: FindRoute, parseParams: ParseParams, invoke: InvokeMethod, send: Send, reject: Reject);
     handle(context: RequestContext): Promise<void>;
 }
