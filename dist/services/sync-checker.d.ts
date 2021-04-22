b454fb022c8d1ff0b194e7a03f5670f2dd3c7d67
Alex Firmani <jafirmani@gmail.com>
2021-04-21 22:00:21 -0700

    sync check structure with lock

--- dist/services/sync-checker.d.ts
+++ dist/services/sync-checker.d.ts
@@ -1,7 +1,7 @@
-import { Node } from '@pokt-network/pocket-js';
+import { Configuration, Node, Pocket } from '@pokt-network/pocket-js';
 import { Redis } from 'ioredis';
 export declare class SyncChecker {
     redis: Redis;
     constructor(redis: Redis);
-    consensusFilter(nodes: Node[], syncCheck: string): Promise<Node[]>;
+    consensusFilter(nodes: Node[], syncCheck: string, blockchain: string, pocket: Pocket, pocketConfiguration: Configuration): Promise<Node[]>;
 }
--- dist/services/sync-checker.js
+++ dist/services/sync-checker.js
@@ -1,15 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var crypto = require('crypto');
 const logger = require('../services/logger');
 class SyncChecker {
     constructor(redis) {
         this.redis = redis;
     }
-    async consensusFilter(nodes, syncCheck) {
+    async consensusFilter(nodes, syncCheck, blockchain, pocket, pocketConfiguration) {
         let syncedNodes = [];
+        // Key is blockchain - a hash of the nodes sorted by public key
+        const syncedNodesKey = blockchain + '-' + crypto.createHash('sha256').update(JSON.stringify(nodes.sort((a, b) => (a.publicKey > b.publicKey) ? 1 : ((b.publicKey > a.publicKey) ? -1 : 0)), (k, v) => k != 'publicKey' ? v : undefined)).digest('hex');
+        const syncedNodesCache = await this.redis.get(syncedNodesKey);
+        if (syncedNodesCache) {
+            return JSON.parse(syncedNodesCache);
+        }
+        // Check lock key; if lock key exists, return full node set
+        const syncLock = await this.redis.get('lock-' + syncedNodesKey);
+        if (syncLock) {
+            return nodes;
+        }
+        else {
+            // Set lock as this thread checks the sync with 10 second ttl
+            await this.redis.set('lock-' + syncedNodesKey, 'true', 'EX', 10);
+        }
+        // Check sync of nodes with consensus
         for (const node of nodes) {
             /*
-            const relayResponse = await this.pocket.sendRelay(
+            const relayResponse = await pocket.sendRelay(
               '{"method":"web3_clientVersion","id":1,"jsonrpc":"2.0"}',
               blockchain,
               pocketAAT,
@@ -31,7 +48,8 @@ class SyncChecker {
             */
             syncedNodes.push(node);
         }
-        logger.log('info', 'SYNC CHECK: ' + syncCheck + ' - results: ' + syncedNodes.length + ' nodes in consensus sync');
+        logger.log('info', 'SYNC CHECK: writing sync status ' + syncedNodesKey);
+        await this.redis.set(syncedNodesKey, JSON.stringify(syncedNodes), 'EX', 300);
         return syncedNodes;
     }
 }
