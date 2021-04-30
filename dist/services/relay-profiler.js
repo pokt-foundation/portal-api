9bc982e500da95927d3a53e7e0956c1fc7e8276b
Alex Firmani <jafirmani@gmail.com>
2021-04-30 14:32:08 -0700

    fixing profile data format

--- dist/services/relay-profiler.js
+++ dist/services/relay-profiler.js
@@ -11,15 +11,16 @@ class RelayProfiler extends pocket_js_1.BaseProfiler {
     }
     flushResults(requestID, functionName, results) {
         const bulkData = [];
+        const timestamp = new Date();
         results.forEach((result) => {
             bulkData.push([
+                timestamp,
                 requestID,
                 functionName,
                 result.blockKey,
                 result.timeElapsed,
             ]);
         });
-        logger.log('info', 'FLUSHING BULK: ' + bulkData.length, { requestID: '', relayType: '', typeID: '', serviceNode: '', error: '', elapsedTime: '' });
         if (bulkData.length > 0) {
             const metricsQuery = pgFormat('INSERT INTO profile VALUES %L', bulkData);
             logger.log('info', 'FLUSHING QUERY: ' + JSON.stringify(metricsQuery), { requestID: '', relayType: '', typeID: '', serviceNode: '', error: '', elapsedTime: '' });
@@ -28,10 +29,9 @@ class RelayProfiler extends pocket_js_1.BaseProfiler {
                     logger.log('error', 'Error acquiring client ' + err.stack);
                 }
                 client.query(metricsQuery, (err, result) => {
-                    logger.log('info', 'FLUSHING RESULT: ' + JSON.stringify(result), { requestID: '', relayType: '', typeID: '', serviceNode: '', error: '', elapsedTime: '' });
                     release();
                     if (err) {
-                        logger.log('error', 'Error executing query ' + metricsQuery + ' ' + err.stack);
+                        logger.log('error', 'FLUSHING Error executing query ' + metricsQuery + ' ' + err.stack);
                     }
                 });
             });
