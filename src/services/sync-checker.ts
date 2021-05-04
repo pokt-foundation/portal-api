f6219a6c84d76a728770792247826a4728e307be
Alex Firmani <jafirmani@gmail.com>
2021-05-04 09:55:40 -0700

    adding more logging around sync check

--- src/services/sync-checker.ts
+++ src/services/sync-checker.ts
@@ -67,7 +67,7 @@ export class SyncChecker {
         // Create a NodeSyncLog for each node with current block
         const nodeSyncLog = {node: node, blockchain: blockchain, blockHeight: parseInt(payload.result, 16)} as NodeSyncLog;
         nodeSyncLogs.push(nodeSyncLog);
-        logger.log('info', 'SYNC CHECK RESULT: ' + JSON.stringify(nodeSyncLog), {requestID: '', relayType: '', typeID: '', serviceNode: node.publicKey});
+        logger.log('info', 'SYNC CHECK RESULT: ' + JSON.stringify(nodeSyncLog), {requestID: '', relayType: '', typeID: '', serviceNode: node.publicKey, error: '', elapsedTime: ''});
       }
       else {
         logger.log('error', 'SYNC CHECK ERROR: ' + JSON.stringify(relayResponse), {requestID: 'synccheck', relayType: '', typeID: '', serviceNode: node.publicKey, error: '', elapsedTime: ''});
@@ -104,11 +104,12 @@ export class SyncChecker {
 
     // Go through nodes and add all nodes that are current or within 1 block -- this allows for block processing times
     for (const nodeSyncLog of nodeSyncLogs) {
-      logger.log('info', 'SYNC CHECK RESULT: ' + nodeSyncLog.node.publicKey + ' height: ' + nodeSyncLog.blockHeight, {requestID: '', relayType: '', typeID: '', serviceNode: nodeSyncLog.node.publicKey, error: '', elapsedTime: ''});
-
       if ((nodeSyncLog.blockHeight + 1) >= currentBlockHeight) {
+        logger.log('info', 'SYNC CHECK IN-SYNC: ' + nodeSyncLog.node.publicKey + ' height: ' + nodeSyncLog.blockHeight, {requestID: '', relayType: '', typeID: '', serviceNode: nodeSyncLog.node.publicKey, error: '', elapsedTime: ''});
         syncedNodes.push(nodeSyncLog.node);
         syncedNodesList.push(nodeSyncLog.node.publicKey);
+      } else {
+        logger.log('info', 'SYNC CHECK BEHIND: ' + nodeSyncLog.node.publicKey + ' height: ' + nodeSyncLog.blockHeight, {requestID: '', relayType: '', typeID: '', serviceNode: nodeSyncLog.node.publicKey, error: '', elapsedTime: ''});
       }
     }
 
