29d2f5fc710993aa9c982c26adc74a88d7477870
Alex Firmani <jafirmani@gmail.com>
2021-05-17 16:49:44 -0700

    logz.io test

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,41 +1,18 @@
 import {HttpErrors} from '@loopback/rest';
+import LogzioWinstonTransport from 'winston-logzio';
 
 require("dotenv").config();
 
 const { createLogger, format, transports } = require('winston');
 const { printf } = format;
-const S3StreamLogger = require('s3-streamlogger').S3StreamLogger;
 
-const s3AccessKeyID: string = process.env.AWS_S3_ACCESS_KEY_ID ?? '';
-const s3SecretAccessKey: string = process.env.AWS_S3_SECRET_ACCESS_KEY ?? '';
-const s3LogsRegion: string = process.env.AWS_S3_LOGS_REGION ?? '';
-const s3LogsBucket: string = process.env.AWS_S3_LOGS_BUCKET ?? '';
-const s3LogsFolder: string = process.env.AWS_S3_LOGS_FOLDER ?? '';
+const logzToken: string = process.env.LOGZ_TOKEN ?? '';
 
-if (!s3AccessKeyID) {
+if (!logzToken) {
   throw new HttpErrors.InternalServerError(
-    'AWS_S3_ACCESS_KEY_ID required in ENV',
+    'LOGZ_TOKEN required in ENV',
   );
 }
-if (!s3SecretAccessKey) {
-  throw new HttpErrors.InternalServerError(
-    'AWS_S3_SECRET_ACCESS_KEY required in ENV',
-  );
-}
-if (!s3LogsBucket) {
-  throw new HttpErrors.InternalServerError(
-    'AWS_S3_LOGS_BUCKET required in ENV',
-  );
-}
-if (!s3LogsFolder) {
-  throw new HttpErrors.InternalServerError(
-    'AWS_S3_LOGS_FOLDER required in ENV',
-  );
-}
-
-const s3StreamInfo = generateS3Logger('/info');
-const s3StreamError = generateS3Logger('/error');
-const s3StreamDebug =  generateS3Logger('/debug');
 
 const timestampUTC = () => {
   const timestamp = new Date();
@@ -73,6 +50,13 @@ const debugFilter = format((log:Log, opts:any) => {
   return log.level === 'debug' ? log : false;
 });
 
+const logzioWinstonTransport = new LogzioWinstonTransport({
+  level: 'info',
+  name: 'winston_logzio',
+  token: logzToken,
+  host: 'listener-uk.logz.io',
+});
+
 const options = {
   console: {
     level: 'info',
@@ -87,52 +71,7 @@ const options = {
       consoleFormat,
     ),
   },
-  s3Info: {
-    level: 'info',
-    handleExceptions: true,
-    colorize: false,
-    stream: s3StreamInfo,
-    format: format.combine(
-      jsonFormat,
-    ),
-  },
-  s3Error: {
-    level: 'error',
-    handleExceptions: true,
-    colorize: false,
-    stream: s3StreamError,
-    format: format.combine(
-      jsonFormat,
-    ),
-  },
-  s3Debug: {
-    level: 'debug',
-    handleExceptions: true,
-    colorize: false,
-    stream: s3StreamDebug,
-    format: format.combine(
-      debugFilter(),
-      jsonFormat,
-    ),
-  },
 };
-
-function generateS3Logger(folder:string): any {
-  const s3StreamLogger = new S3StreamLogger({
-    bucket: s3LogsBucket,
-    folder: s3LogsFolder + folder,
-    region: s3LogsRegion,
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    access_key_id: s3AccessKeyID,
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    secret_access_key: s3SecretAccessKey,
-  });
-  
-  s3StreamLogger.on('error', function(err: Error){
-    console.log('error', 'S3 logging error', err);
-  });
-  return s3StreamLogger;
-}
 
 interface Log {
   level: string;
@@ -146,11 +85,12 @@ interface Log {
 }
 
 module.exports = createLogger({
+  format: format.combine(
+    jsonFormat,
+  ),
   transports: [
     new transports.Console(options.console),
-    new (transports.Stream)(options.s3Info),
-    new (transports.Stream)(options.s3Error),
-    new (transports.Stream)(options.s3Debug),
+    logzioWinstonTransport,
   ],
   exitOnError: false,
 });
