be00ae6be6a44b7acec4bc6264a4fcb3ccbf578f
Alex Firmani <jafirmani@gmail.com>
2021-06-29 09:57:35 -0700

    making scripts use diff connection strings per instance

--- scripts/import-production-data.sh
+++ scripts/import-production-data.sh
@@ -8,17 +8,17 @@ echo "Waiting...";
 
 sleep 5s
 
-echo "Persisting production collections";
+echo "Importing production collections";
 
 for collection in "Applications" "Blockchains" "CronJobData" "LoadBalancers" "Nodes" "PaymentHistory" "PaymentMethods" "PendingTransactions" "Users"; \
   do \
-    echo "Persisting $collection collection..."; \
+    echo "Importing $collection collection..."; \
 
     mongoimport \
-        --uri $MONGO_PROTOCOL://$MONGO_DEST_USER:$MONGO_DEST_PW@$MONGO_DEST_HOST/$MONGO_DEST_DB \
+        --uri $MONGO_DEST_PROTOCOL://$MONGO_DEST_USER:$MONGO_DEST_PW@$MONGO_DEST_HOST/$MONGO_DEST_DB \
         --collection "$collection" \
         --type json \
         --file /data/$collection; \
 
-    echo "Persisted $collection collection successfully..."; \
+    echo "Imported $collection collection successfully..."; \
 done;
