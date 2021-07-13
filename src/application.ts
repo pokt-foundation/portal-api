bddc518b3d833a088e787f1823587313b7ba33e1
Roniel Valdez <roniel_valdez@outlook.com>
2021-07-13 13:57:29 -0400

    change null coalescing operator from env variables

--- src/application.ts
+++ src/application.ts
@@ -44,19 +44,19 @@ export class PocketGatewayApplication extends BootMixin(ServiceMixin(RepositoryM
     // Requirements; for Production these are stored in GitHub repo secrets
     //
     // For Dev, you need to pass them in via .env file
-    const environment: string = process.env.NODE_ENV ?? 'production'
+    const environment: string = process.env.NODE_ENV || 'production'
 
     logger.log('info', 'Environment: ' + environment)
 
-    const dispatchURL: string = process.env.DISPATCH_URL ?? ''
-    const altruists: string = process.env.ALTRUISTS ?? ''
-    const clientPrivateKey: string = process.env.GATEWAY_CLIENT_PRIVATE_KEY ?? ''
-    const clientPassphrase: string = process.env.GATEWAY_CLIENT_PASSPHRASE ?? ''
-    const pocketSessionBlockFrequency: string = process.env.POCKET_SESSION_BLOCK_FREQUENCY ?? ''
-    const pocketBlockTime: string = process.env.POCKET_BLOCK_TIME ?? ''
-    const relayRetries: string = process.env.POCKET_RELAY_RETRIES ?? ''
-    const databaseEncryptionKey: string = process.env.DATABASE_ENCRYPTION_KEY ?? ''
-    const aatPlan = process.env.AAT_PLAN ?? AatPlans.PREMIUM
+    const dispatchURL: string = process.env.DISPATCH_URL || ''
+    const altruists: string = process.env.ALTRUISTS || ''
+    const clientPrivateKey: string = process.env.GATEWAY_CLIENT_PRIVATE_KEY || ''
+    const clientPassphrase: string = process.env.GATEWAY_CLIENT_PASSPHRASE || ''
+    const pocketSessionBlockFrequency: string = process.env.POCKET_SESSION_BLOCK_FREQUENCY || ''
+    const pocketBlockTime: string = process.env.POCKET_BLOCK_TIME || ''
+    const relayRetries: string = process.env.POCKET_RELAY_RETRIES || ''
+    const databaseEncryptionKey: string = process.env.DATABASE_ENCRYPTION_KEY || ''
+    const aatPlan = process.env.AAT_PLAN || AatPlans.PREMIUM
 
     if (!dispatchURL) {
       throw new HttpErrors.InternalServerError('DISPATCH_URL required in ENV')
@@ -144,8 +144,8 @@ export class PocketGatewayApplication extends BootMixin(ServiceMixin(RepositoryM
     this.bind('redisInstance').to(redis)
 
     // Load Postgres for TimescaleDB metrics
-    const pgConnection: string = process.env.PG_CONNECTION ?? ''
-    const pgCertificate: string = process.env.PG_CERTIFICATE ?? ''
+    const pgConnection: string = process.env.PG_CONNECTION || ''
+    const pgCertificate: string = process.env.PG_CERTIFICATE || ''
 
     if (!pgConnection) {
       throw new HttpErrors.InternalServerError('PG_CONNECTION required in ENV')
