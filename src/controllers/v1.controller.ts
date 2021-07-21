22736a6573df694c9d72be6eb8cbc8214f46118b
Alex Firmani <jafirmani@gmail.com>
2021-07-21 14:11:49 -0700

    adding bare domain redirect system

--- src/controllers/v1.controller.ts
+++ src/controllers/v1.controller.ts
@@ -20,6 +20,7 @@ export class V1Controller {
   pocketRelayer: PocketRelayer
   syncChecker: SyncChecker
   chainChecker: ChainChecker
+  forceBlockchain: string
 
   constructor(
     @inject('secretKey') private secretKey: string,
@@ -40,6 +41,7 @@ export class V1Controller {
     @inject('requestID') private requestID: string,
     @inject('defaultSyncAllowance') private defaultSyncAllowance: number,
     @inject('aatPlan') private aatPlan: string,
+    @inject('redirects') private redirects: string,
     @repository(ApplicationsRepository)
     public applicationsRepository: ApplicationsRepository,
     @repository(BlockchainsRepository)
@@ -78,6 +80,35 @@ export class V1Controller {
       altruists: this.altruists,
       aatPlan: this.aatPlan,
     })
+  }
+
+  /**
+   * Redirect simple URLs to specific Load Balancers
+   */
+  @post('/')
+  async redirect(
+    @requestBody({
+      description: 'Relay Request',
+      required: true,
+      content: {
+        'application/json': {
+          'x-parser': 'raw',
+        },
+      },
+    })
+    rawData: object
+  ): Promise<string | Error> {
+    if (!this.redirects) {
+      return new Error('No redirect domains allowed')
+    }
+
+    for (const redirect of JSON.parse(this.redirects)) {
+      if (this.pocketRelayer.host.toLowerCase().includes(redirect.domain, 0)) {
+        this.pocketRelayer.host = redirect.blockchain
+        return this.loadBalancerRelay(redirect.loadBalancerID, rawData)
+      }
+    }
+    return new Error('Invalid domain')
   }
 
   /**
