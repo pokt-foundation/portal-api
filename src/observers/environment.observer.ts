b3fab13f8ac17d33ac5659d16550df436e45671a
Alex Firmani <jafirmani@gmail.com>
2021-07-25 07:00:27 -0700

    restoring fuse rpc endpoint

--- src/observers/environment.observer.ts
+++ src/observers/environment.observer.ts
@@ -25,6 +25,7 @@ export class EnvironmentObserver implements LifeCycleObserver {
     'LOGZ_TOKEN',
     'DISPATCH_URL',
     'ALTRUISTS',
+    'REDIRECTS',
     'POCKET_SESSION_BLOCK_FREQUENCY',
     'POCKET_BLOCK_TIME',
     'POCKET_RELAY_RETRIES',
