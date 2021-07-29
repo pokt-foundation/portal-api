a2cc9a42649f24184a9de016b1a1c7a3aa679a4a
Roniel Valdez <roniel_valdez@outlook.com>
2021-07-29 17:01:12 -0400

    tests: add pocketjs library mock

---
+++ tests/unit/pocket-relayer.unit.ts
@@ -0,0 +1,21 @@
+// import { expect } from '@loopback/testlab'
+// import RedisMock from 'ioredis-mock'
+// import { PocketRelayer } from '../../src/services/pocket-relayer';
+// import { CherryPicker } from '../../src/services/cherry-picker';
+// import { ChainChecker } from '../../src/services/chain-checker';
+
+// describe('Cherry picker service (unit)', () => {
+//   let pocketRelayer : PocketRelayer
+//   let cherryPicker : CherryPicker
+//   let chainChecker: ChainChecker
+
+//   let redis : RedisMock
+
+//   before('initialize instance', async () => {
+//     redis = new RedisMock(0, '')
+
+//     cherryPicker = new CherryPicker({ redis, checkDebug: false })
+
+//     const chainCHecker = new
+//   })
+// })
