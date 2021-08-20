c789ee23924837f71481ad7f2c1a2e4418beb6cf
Roniel Valdez <roniel_valdez@outlook.com>
2021-08-20 11:03:37 -0400

    feat: [in-progress] winston logs to cloudwatch

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,3 +1,8 @@
+import { string } from 'pg-format'
+import { LogEntry } from 'winston'
+import WinstonCloudwatch from 'winston-cloudwatch'
+import crypto from 'crypto'
+
 require('dotenv').config()
 
 const { createLogger, format, transports: winstonTransports } = require('winston')
@@ -28,23 +33,73 @@ const consoleFormat = printf(
   }
 )
 
+const startTime = new Date().toISOString()
+
+const awsFormat = (
+  level: string,
+  message: string,
+  { requestID, relayType, typeID, serviceNode, error, elapsedTime }: Log
+) =>
+  `[${timestampUTC()}] [${level}] [${requestID}] [${relayType}] [${typeID}] [${serviceNode}] [${error}] [${elapsedTime}] ${message}`
+
+const logFormat = format.combine(
+  format.colorize(),
+  format.simple(),
+  format.timestamp({
+    format: 'YYYY-MM-DD HH:mm:ss.SSS',
+  }),
+  consoleFormat
+)
+
+const logGroup = `${process.env.REGION_NAME || ''}/ecs/gateway`
+
 const options = {
   console: {
     level: 'debug',
     handleExceptions: true,
     colorize: true,
-    format: format.combine(
-      format.colorize(),
-      format.simple(),
-      format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss.SSS',
-      }),
-      consoleFormat
-    ),
+    format: logFormat,
+  },
+  aws: {
+    name: 'cloudwatch-log',
+    logGroupName: logGroup,
+    logStreamName: function () {
+      // Spread log streams across dates as the server stays up
+      const date = new Date().toISOString().split('T')[0]
+
+      return logGroup + date + '-' + crypto.createHash('md5').update(startTime).digest('hex')
+    },
+    awsRegion: process.env.REGION,
+    awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    awsSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
+    level: 'verbose',
+    messageFormatter: (logObject: LogEntry) => {
+      const { level, requestID, relayType, typeID, serviceNode, error, elapsedTime, message } = logObject
+
+      return JSON.stringify({
+        timestamp: timestampUTC(),
+        level,
+        requestID,
+        relayType,
+        typeID,
+        serviceNode,
+        error,
+        elapsedTime,
+        message,
+      })
+    },
   },
 }
 
-const getTransports = (env: string) => [new winstonTransports.Console(options.console)]
+const getTransports = (env: string) => {
+  const transports = [new winstonTransports.Console(options.console)]
+
+  if (environment === 'production') {
+    transports.push(new WinstonCloudwatch(options.aws))
+  }
+
+  return transports
+}
 
 const perEnvTransports = getTransports(environment)
 
