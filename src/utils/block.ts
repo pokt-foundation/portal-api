6c332f348fa4673bd55214d0538b0688416829f2
crisog <ortega.cpp@gmail.com>
2021-10-04 18:05:24 -0400

    feat: isblockhex utility function

--- src/utils/block.ts
+++ src/utils/block.ts
@@ -1,3 +1,7 @@
 export function blockHexToDecimal(hex: string): number {
   return parseInt(hex, 16)
 }
+
+export function isBlockHex(block: string): boolean {
+  return /^[A-F0-9]+$/i.test(block)
+}
