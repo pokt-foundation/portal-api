40536bab857516e50b7c27de8e5ddc508495145f
crisog <ortega.cpp@gmail.com>
2021-10-06 16:48:17 -0400

    feat: add blockchainID to limit error logs

--- src/services/pocket-relayer.ts
+++ src/services/pocket-relayer.ts
@@ -149,7 +149,7 @@ export class PocketRelayer {
     const data = JSON.stringify(parsedRawData)
 
     if (limitation instanceof Error) {
-      logger.log('error', `Limitation Error req: ${data}`, {
+      logger.log('error', `Limitation Error ${blockchainID} req: ${data}`, {
         requestID: requestID,
         relayType: 'APP',
         error: `${parsedRawData.method} method limitations exceeded.`,
