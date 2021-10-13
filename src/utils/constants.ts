74997ae9aea70dc7f8ff458623bb07c099e4b60f
crisog <ortega.cpp@gmail.com>
2021-10-13 14:29:29 -0400

    ref; alphabetize constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
+export const MAX_RELAYS_ERROR = 'the evidence is sealed, either max relays reached or claim already submitted'
+
 export const WS_ONLY_METHODS = [
-  'eth_subscribe',
-  'eth_newFilter',
-  'newBlockFilter',
   'eth_getFilterChanges',
   'eth_getFilterLogs',
+  'eth_newFilter',
+  'eth_subscribe',
+  'newBlockFilter',
 ]
-
-export const MAX_RELAYS_ERROR = 'the evidence is sealed, either max relays reached or claim already submitted'
