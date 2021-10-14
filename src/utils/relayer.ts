e06b7f698285de1a4f11f7de125a759217506819
crisog <ortega.cpp@gmail.com>
2021-10-14 18:37:49 -0400

    ref; invert if logic

--- src/utils/relayer.ts
+++ src/utils/relayer.ts
@@ -54,55 +54,55 @@ export async function loadBlockchain(
     (b: { blockchain: string }) => b.blockchain.toLowerCase() === blockchainRequest.toLowerCase()
   )
 
-  if (blockchainFilter) {
-    let blockchainEnforceResult = ''
-    let blockchainIDCheck = ''
-    let blockchainID = ''
-    let blockchainChainID = ''
-    let blockchainLogLimitBlocks = DEFAULT_BLOCK_LOGS_LIMIT
-    const blockchainSyncCheck = {} as SyncCheckOptions
-
-    const blockchain = blockchainFilter.blockchain // ex. 'eth-mainnet'
-
-    blockchainID = blockchainFilter.hash as string // ex. '0021'
-
-    // Record the necessary format for the result; example: JSON
-    if (blockchainFilter.enforceResult) {
-      blockchainEnforceResult = blockchainFilter.enforceResult
-    }
-    // Sync Check to determine current blockheight
-    if (blockchainFilter.syncCheckOptions) {
-      blockchainSyncCheck.body = (blockchainFilter.syncCheckOptions.body || '').replace(/\\"/g, '"')
-      blockchainSyncCheck.resultKey = blockchainFilter.syncCheckOptions.resultKey || ''
-
-      // Sync Check path necessary for some chains
-      blockchainSyncCheck.path = blockchainFilter.syncCheckOptions.path || ''
-
-      // Allowance of blocks a data node can be behind
-      blockchainSyncCheck.allowance = parseInt(blockchainFilter.syncCheckOptions.allowance || 0)
-    }
-    // Chain ID Check to determine correct chain
-    if (blockchainFilter.chainIDCheck) {
-      blockchainIDCheck = blockchainFilter.chainIDCheck.replace(/\\"/g, '"')
-      blockchainChainID = blockchainFilter.chainID // ex. '100' (xdai) - can also be undefined
-    }
-    // Max number of blocks to request logs for, if not available, result to env
-    if ((blockchainFilter.logLimitBlocks as number) > 0) {
-      blockchainLogLimitBlocks = parseInt(blockchainFilter.logLimitBlocks)
-    } else if (defaultLogLimitBlocks > 0) {
-      blockchainLogLimitBlocks = defaultLogLimitBlocks
-    }
-
-    return Promise.resolve({
-      blockchain,
-      blockchainEnforceResult,
-      blockchainSyncCheck,
-      blockchainIDCheck,
-      blockchainID,
-      blockchainChainID,
-      blockchainLogLimitBlocks,
-    } as BlockchainDetails)
-  } else {
+  if (!blockchainFilter) {
     throw new HttpErrors.BadRequest(`Incorrect blockchain: ${host}`)
   }
+
+  let blockchainEnforceResult = ''
+  let blockchainIDCheck = ''
+  let blockchainID = ''
+  let blockchainChainID = ''
+  let blockchainLogLimitBlocks = DEFAULT_BLOCK_LOGS_LIMIT
+  const blockchainSyncCheck = {} as SyncCheckOptions
+
+  const blockchain = blockchainFilter.blockchain // ex. 'eth-mainnet'
+
+  blockchainID = blockchainFilter.hash as string // ex. '0021'
+
+  // Record the necessary format for the result; example: JSON
+  if (blockchainFilter.enforceResult) {
+    blockchainEnforceResult = blockchainFilter.enforceResult
+  }
+  // Sync Check to determine current blockheight
+  if (blockchainFilter.syncCheckOptions) {
+    blockchainSyncCheck.body = (blockchainFilter.syncCheckOptions.body || '').replace(/\\"/g, '"')
+    blockchainSyncCheck.resultKey = blockchainFilter.syncCheckOptions.resultKey || ''
+
+    // Sync Check path necessary for some chains
+    blockchainSyncCheck.path = blockchainFilter.syncCheckOptions.path || ''
+
+    // Allowance of blocks a data node can be behind
+    blockchainSyncCheck.allowance = parseInt(blockchainFilter.syncCheckOptions.allowance || 0)
+  }
+  // Chain ID Check to determine correct chain
+  if (blockchainFilter.chainIDCheck) {
+    blockchainIDCheck = blockchainFilter.chainIDCheck.replace(/\\"/g, '"')
+    blockchainChainID = blockchainFilter.chainID // ex. '100' (xdai) - can also be undefined
+  }
+  // Max number of blocks to request logs for, if not available, result to env
+  if ((blockchainFilter.logLimitBlocks as number) > 0) {
+    blockchainLogLimitBlocks = parseInt(blockchainFilter.logLimitBlocks)
+  } else if (defaultLogLimitBlocks > 0) {
+    blockchainLogLimitBlocks = defaultLogLimitBlocks
+  }
+
+  return Promise.resolve({
+    blockchain,
+    blockchainEnforceResult,
+    blockchainSyncCheck,
+    blockchainIDCheck,
+    blockchainID,
+    blockchainChainID,
+    blockchainLogLimitBlocks,
+  } as BlockchainDetails)
 }
