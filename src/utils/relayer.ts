a79377fcc03bec1fef8ccfb33662eb755e06206d
crisog <ortega.cpp@gmail.com>
2021-12-02 16:03:18 -0400

    hotfix: unused param

--- src/utils/relayer.ts
+++ src/utils/relayer.ts
@@ -55,7 +55,7 @@ export async function loadBlockchain(
   )
 
   if (!blockchainFilter) {
-    throw new ErrorObject(1, new jsonrpc.JsonRpcError(`Incorrect blockchain: ${host}`, -32057))
+    throw new ErrorObject(rpcID, new jsonrpc.JsonRpcError(`Incorrect blockchain: ${host}`, -32057))
   }
 
   let blockchainEnforceResult = ''
