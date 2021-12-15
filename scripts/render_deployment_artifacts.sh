0a9e3cbb43aaa2ae74b7678d3c646511a5240201
Roniel Valdez <roniel_valdez@outlook.com>
2021-12-15 11:13:48 -0500

    infra: add deployment files shared envs

---
+++ scripts/render_deployment_artifacts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# adds shared data to deployment files to mainly avoid misconfiguration due to
+# copy/pasting
+
+github_path=".github/workflows"
+template_path="pocket-gateway/tasks"
+shared_data="shared-envs.json"
+
+patterns=("canary*.yml" "production*.yml", "staging*.yml")
+
+for pattern in "${patterns[@]}" 
+  do
+  patternFiles=$(find $template_path  -name $pattern)
+  for file in $patternFiles
+  do
+    name=$(basename $file)
+    destination_path=$github_path/$name
+
+    mustache $template_path/$shared_data $file \
+    > $destination_path
+
+    git add $destination_path
+  done
+done
