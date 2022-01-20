82fca6b5635c54c77e1d5f3d577c198e78ba2e04
Roniel Valdez <roniel_valdez@outlook.com>
2022-01-20 09:19:42 -0400

    hotfix: linter for yaml; local setup remove orphans

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.(t|j)s': ['eslint --fix', 'prettier --write .'],
+  '*.((t|j)s|yml|yaml)': ['eslint --fix', 'prettier --write .'],
   // Applies shared environments to all deployment artifacts
   '*pocket-gateway/**/*(shared-envs.json|*.yml)': 'npm run tasks:deploy:render-artifacts',
 }
