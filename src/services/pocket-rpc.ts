8b9876264980feaa72e920884ffb8521beaf110f
Roniel Valdez <roniel_valdez@outlook.com>
2022-02-01 10:31:39 -0500

    chore: add dispatcher log

--- src/services/pocket-rpc.ts
+++ src/services/pocket-rpc.ts
@@ -10,6 +10,8 @@ export class PocketRPC {
 
   constructor(dispatchers: URL[]) {
     this.dispatchers = dispatchers
+
+    console.log('DISPARCHERS _____----:', this.dispatchers)
   }
 
   async dispatchNewSession({
@@ -37,6 +39,13 @@ export class PocketRPC {
       // Doing the rpc call directly minimizes the possibily of failing due to timeouts
       dispatcher = this.pickRandomDispatcher()
       const dispatchURL = `${dispatcher}v1/client/dispatch`
+
+      logger.log('info', 'Dispatcher information', {
+        dispatcherList: this.dispatchers.map((dist) => dist.toString()),
+        dispatchURL: dispatchURL,
+        requestID,
+        applicationID,
+      })
 
       try {
         dispatchResponse = await axios.post(
