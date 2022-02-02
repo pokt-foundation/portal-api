71054ce03d82b44d84edd1d52e93b29cd13bed06
Roniel Valdez <roniel_valdez@outlook.com>
2022-02-02 14:47:41 -0500

    fix: package update errors

--- src/services/metrics-recorder.ts
+++ src/services/metrics-recorder.ts
@@ -1,5 +1,5 @@
 import process from 'process'
-import { CustomLogger } from 'ajv'
+import { Logger } from 'ajv'
 import { Redis } from 'ioredis'
 import { Pool as PGPool } from 'pg'
 
@@ -248,7 +248,7 @@ export class MetricsRecorder {
     bulkData: any[],
     currentTimestamp: number,
     redisKey: string,
-    processlogger: CustomLogger
+    processlogger: Logger
   ): Promise<void> {
     const redisListAge = await this.redis.get('age-' + redisKey)
     const redisListSize = await this.redis.llen(redisKey)
@@ -271,7 +271,7 @@ export class MetricsRecorder {
     bulkData: any[],
     redisListSize: number,
     redisKey: string,
-    processlogger: CustomLogger
+    processlogger: Logger
   ): Promise<void> {
     for (let count = 0; count < redisListSize; count++) {
       const redisRecord = await this.redis.lpop(redisKey)
