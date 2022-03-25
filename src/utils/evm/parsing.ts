26540b4b9c17391a37435f57f55ea64e940a92f4
crisog <ortega.cpp@gmail.com>
2022-03-25 06:55:10 -0400

    feat: evm parsing utils

---
+++ src/utils/evm/parsing.ts
@@ -0,0 +1,44 @@
+import { ethers } from 'ethers'
+import { parseMethod } from '../parsing'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function extractContractAddress(rawData: Record<string, any>) {
+  const [firstParameter] = rawData.params
+  const method = parseMethod(rawData)
+
+  if (method === 'eth_sendRawTransaction') {
+    // firstParameter is the raw tx hex
+    return decodeEthRawTxAddress(firstParameter)
+  } else if (method === 'eth_call') {
+    return firstParameter?.to
+  } else if (method === 'eth_getLogs') {
+    return firstParameter?.address
+  } else if (
+    method === 'eth_getCode' ||
+    method === 'eth_getBalance' ||
+    method === 'eth_getStorageAt' ||
+    method === 'eth_getTransactionCount'
+  ) {
+    // firstParameter is the address
+    return firstParameter
+  }
+
+  // If the method is not supported, return undefined
+  return undefined
+}
+
+export function decodeEthRawTxAddress(rawTxBytes: string) {
+  const decodedTx = ethers.utils.RLP.decode(rawTxBytes)
+
+  const [
+    ,
+    ,
+    ,
+    // rawNonce
+    // rawGasPrice
+    // rawGasLimit
+    rawTo,
+  ] = decodedTx
+
+  return rawTo
+}
