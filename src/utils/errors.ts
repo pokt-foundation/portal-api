0d0b80e618e6c5ee33949a23b533315970927975
crisog <ortega.cpp@gmail.com>
2022-05-13 15:05:29 -0400

    fix: parse json rpc err

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,22 +1,11 @@
 import { parseJSONRPCError } from './parsing'
 
-export const EVM_ERROR_CODES = [
-  // JSON RPC Standard errors
-  '-32',
-  // Execution reverted error
-  '3',
-]
-
 // EVM clients rely on JsonRpc standard, so we check for those errors.
 export function isEVMError(response: string): boolean {
-  return isError(response, EVM_ERROR_CODES)
-}
-
-function isError(response: string, errorsToCheck: string[]): boolean {
   try {
     const { code } = parseJSONRPCError(response)
 
-    return errorsToCheck.some((error) => String(code).includes(error))
+    return code === 3 || code < -32000
   } catch {
     return false
   }
