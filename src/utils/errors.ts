c607e176125bd18cb7aa698a3a34b6ba0a85669f
crisog <ortega.cpp@gmail.com>
2022-05-13 15:18:54 -0400

    add comments

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -5,6 +5,8 @@ export function isEVMError(response: string): boolean {
   try {
     const { code } = parseJSONRPCError(response)
 
+    // 3 is execution error
+    // -32000 itself can be thrown for what are server errors, all other -32000 are user errors
     return code === 3 || code < -32000
   } catch {
     return false
