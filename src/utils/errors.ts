0bf6dead584a307af6fe45a3805e94df114d8584
crisog <ortega.cpp@gmail.com>
2022-05-16 09:09:04 -0400

    imp: filter out specific -32000 user errors

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,13 +1,18 @@
 import { parseJSONRPCError } from './parsing'
 
+const EVM_USER_ERROR_MSGS = ['execution reverted', 'stack underflow', 'cannot be found']
+
 // EVM clients rely on JsonRpc standard, so we check for those errors.
-export function isEVMError(response: string): boolean {
+export function isUserErrorEVM(response: string): boolean {
   try {
-    const { code } = parseJSONRPCError(response)
+    const { code, message } = parseJSONRPCError(response)
+
+    const userError = EVM_USER_ERROR_MSGS.some((err) => message.includes(err))
 
     // 3 is execution error
-    // -32000 itself can be thrown for what are server errors, all other -32000 are user errors
-    return code === 3 || code < -32000
+    // -32000 itself can be thrown for what are server errors (except for execution reverted & stack underflow)
+    // all other -32000 are user errors
+    return userError || code === 3 || code < -32000
   } catch {
     return false
   }
