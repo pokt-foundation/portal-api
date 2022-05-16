7dd71db66c755208607f3638e170cd1e0a5077e8
crisog <ortega.cpp@gmail.com>
2022-05-16 11:30:51 -0400

    add comment

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,5 +1,6 @@
 import { parseJSONRPCError } from './parsing'
 
+// some clients use -32000 for both server/user errors, so message match is needed
 const EVM_USER_ERROR_MSGS = ['execution reverted', 'stack underflow', 'cannot be found']
 
 // EVM clients rely on JsonRpc standard, so we check for those errors.
