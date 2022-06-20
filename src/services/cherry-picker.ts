032d9d4305f93393961d44e1e332c7189832f4fa
crisog <ortega.cpp@gmail.com>
2022-06-20 15:34:32 -0400

    test

--- src/services/cherry-picker.ts
+++ src/services/cherry-picker.ts
@@ -8,7 +8,7 @@ const logger = require('../services/logger')
 
 const logStats = (process.env['LOG_CHERRY_PICKER_STATS'] || '').toLowerCase() === 'true'
 
-// Amount of times a node  to 
+// Amount of times a   to 
 // being removed from the session
 const TIMEOUT_LIMIT = 20
 
