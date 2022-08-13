ab9ed81577dccb26569bd306c5f88208fd3f1cd2
Arash Deshmeh <adshmh@gmail.com>
2022-08-13 09:48:07 -0400

    Fix: check for 0x02 prefix indicating EIP1559
    Signed-off-by: Arash Deshmeh <adshmh@gmail.com>

--- src/utils/evm/parsing.ts
+++ src/utils/evm/parsing.ts
@@ -29,7 +29,7 @@ export function extractContractAddress(rawData: Record<string, any>): string | u
 export function decodeEthRawTxAddress(rawTxBytes: string): string {
   // Fix for parsing EIP1559: see below link for more details
   // https://github.com/ethers-io/ethers.js/discussions/3269
-  if (rawTxBytes.substring(2, 4) === '02') {
+  if (rawTxBytes.substring(0, 4) === '0x02') {
     rawTxBytes = rawTxBytes.substring(0, 2) + rawTxBytes.substring(4)
   }
 
