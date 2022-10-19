281b4f9bf8641c50afe3dbf90cbec58fd9e31345
crisog <ortega.cpp@gmail.com>
2022-10-19 11:37:38 -0500

    feat: add payload to log

--- src/controllers/v1.controller.ts
+++ src/controllers/v1.controller.ts
@@ -143,6 +143,7 @@ export class V1Controller {
       const [blockchainRequest] = this.host.split('.')
 
       logger.log('info', `PUBLIC RPC RELAY REDIRECTED FOR ${blockchainRequest}`, {
+        requestBody: parsedRawData,
         blockchainSubdomain: blockchainRequest,
       })
 
