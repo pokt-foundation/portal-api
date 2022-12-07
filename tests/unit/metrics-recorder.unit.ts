99d371bd1813cb10c960aa7ecdfabb76c41b7164
crisog <ortega.cpp@gmail.com>
2022-12-07 11:07:34 -0400

    tests: metric recorder unit

---
+++ tests/unit/metrics-recorder.unit.ts
@@ -0,0 +1,23 @@
+import { expect } from '@loopback/testlab'
+
+describe('Metrics recorder (unit)', () => {
+  it('Should reduce multi-method calls for metrics/logging purposes', () => {
+    let method = 'eth_getTransactionReceipt,eth_getTransactionByHash,eth_getBlockByNumber'
+
+    if (method && method.split(',').length > 1) {
+      method = 'multiple'
+    }
+
+    expect(method).to.to.be.equal('multiple')
+  })
+
+  it('Should not reduce single method call for metrics/logging purposes', () => {
+    let method = 'eth_getBlockByNumber'
+
+    if (method && method.split(',').length > 1) {
+      method = 'multiple'
+    }
+
+    expect(method).to.to.be.equal('eth_getBlockByNumber')
+  })
+})
