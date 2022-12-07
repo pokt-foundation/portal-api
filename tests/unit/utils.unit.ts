e924cd7db02ab031196157788add8228940920bb
crisog <ortega.cpp@gmail.com>
2022-12-07 13:34:21 -0400

    tests: renaming unit test

--- tests/unit/utils.unit.ts
+++ tests/unit/utils.unit.ts
@@ -1,6 +1,6 @@
 import { expect } from '@loopback/testlab'
 
-describe('Metrics recorder (unit)', () => {
+describe('Utility (unit)', () => {
   it('Should reduce multi-method calls for metrics/logging purposes', () => {
     let method = 'eth_getTransactionReceipt,eth_getTransactionByHash,eth_getBlockByNumber'
 
